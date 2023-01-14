53cec06c9994a009736f9444700a17f6706fcf76
zchdvs <zchdvs232@gmail.com>
2023-01-13 21:13:13 -0800

    update for vercel CI/CD

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 
 app.include_router(discord.router)
 app.include_router(midjourney.router)
+
+import uvicorn
+
+if __name__ == "__main__":
+  uvicorn.run("server.api:app", host="0.0.0.0", port=8000, reload=True)
